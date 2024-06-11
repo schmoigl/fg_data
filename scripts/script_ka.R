5cea9874b504e54b5de40e76380664821547c646
Lukas Schmoigl <lukas.schmoigl@wifo.ac.at>
2024-06-11 13:31:35 +0200

    added flash

--- scripts/script_ka.R
+++ scripts/script_ka.R
@@ -34,3 +34,36 @@ zsp <- read_excel(paste0(
   drop_na()
 
 write_csv(zsp, file = "K:/Gitea/fg_data/data_ka.csv")
+
+
+## KT flash
+
+version <- format(Sys.time(), "%Y%m")
+# version <- "202405"
+
+ampel <- read_excel(paste0(
+  "//int.wsr.at/Nabu/Themen/Surveys/Konjunkturtest/ktflash/Flash",
+  version,
+  "/ampel.xlsx"
+  )) %>%
+  rename(date = ...1) %>%
+  select(date, ampel) %>%
+  drop_na()
+
+zsp <- read_excel(paste0(
+  "//int.wsr.at/Nabu/Themen/Surveys/Konjunkturtest/ktflash/Flash",
+  version,
+  "/Zsp_Indices_", 
+  version,
+  "_r.xlsx"
+  )) %>%
+  rename(
+    date = ...1,
+    aktuell = ECON_CONTEMPORARY_INDEX,
+    erwartet = ECON_EXPECTATIONS_INDEX,
+    econclimate = ECON_CLIMATE
+  ) %>%
+  left_join(ampel) %>%
+  drop_na()
+
+write_csv(zsp, file = "K:/Gitea/fg_data/data_ka_flash.csv")
