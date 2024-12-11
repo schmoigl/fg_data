e1b7c65ae176f8f6250143fe8271da0b10fbe46b
Lukas Schmoigl <lschmoigl@wsr.ac.at>
2024-12-11 16:56:18 +0100

    working version on clio

--- scripts/push_clio.sh
+++ scripts/push_clio.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-echo "Starting"
 cd /home/lschmoigl/Github/fg_data
-
-echo "git pull"
 git pull
-
-echo "pixi run update"
 /home/lschmoigl/.pixi/bin/pixi run update
-
-echo "git"
 git add .
 git commit -m "auto-commit"
 git push
